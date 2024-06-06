c3fd9ba8a550d82c82b1ca1adf7625b6a197c639
Nathaniel M. <me@itznxthaniel.dev>
2024-06-05 22:25:59 -0400

    Locale switcher and en-US doesn't have path prefix.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,8 @@ import locales from './supportedLocales.json';
 
 export default createMiddleware({
   locales: locales,
-  defaultLocale: "en-US"
+  defaultLocale: "en-US",
+  localePrefix: "as-needed"
 });
 
 export const config = {

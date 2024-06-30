73f533f885e9702f6fbc0f6c8d5613cf38c3cf00
RandomAnimeGamer <iobjectgaming@gmail.com>
2024-06-29 18:15:00 -0700

    Fixed theme switcher, added localization dropdown

---
+++ src/components/navbar/language.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import clsx from 'clsx';
+import { useParams, useRouter, usePathname } from 'next/navigation';
+import { ChangeEvent, ReactNode, useTransition, useState } from 'react';
+import { Select } from '@mantine/core';
+import classes from "./navbar.module.scss";
+
+type Props = {
+	data: any;
+	defaultValue: string;
+	pathname: any;
+	translate: any;
+};
+
+const combineParams = (pathname:any, params:any) => {
+	let str = [];
+	for(let key in params) {
+		if(params.hasOwnProperty(key)) {
+			str.push(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]))
+		}
+	}
+	return pathname + "?" + str.join("&");
+}
+
+export default function Lang({ data, defaultValue, pathname, translate }: Props) {
+	const router = useRouter();
+	const [isPending, startTransition] = useTransition();
+	const params = useParams();
+	const translatedData = data.map((lang:any) => {
+		return translate('languages.' + lang);
+	});
+
+	function onSelectChange(event:any, translatedData:any) {
+		const index = translatedData.findIndex((el:any) => { return el == event });
+		const nextLocale = data[index];
+		
+		let url = '';
+		let splitted = pathname.split('/');
+		if(splitted.length > 1) {
+			if(data.includes(splitted[1])) {
+				splitted = splitted.filter((part:any) => {
+					return part !== splitted[1]
+				});
+			}
+			url = '/' + nextLocale + '/' + splitted.join('/');
+		}
+		else url = pathname + nextLocale;
+
+		startTransition(() => { router.replace(url.replace(/(\/\/)+/g, '/')); });
+	}
+    
+    return (
+		<div className={classes.language}>
+			<Select
+				defaultValue={translatedData[data.findIndex((el:any) => { return el == defaultValue })]}
+				disabled={isPending}
+				onChange={(e) => onSelectChange(e, translatedData)}
+				data={translatedData}
+			/>
+		</div>
+    );
+};
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,97 +1,103 @@
 "use client";
 
+import { useState } from 'react';
 import {
   HoverCard,
   Group,
   Button,
-  UnstyledButton,
   Text,
   SimpleGrid,
   ThemeIcon,
   Anchor,
   Divider,
-  Center,
   Box,
   Burger,
   Drawer,
-  Collapse,
   ScrollArea,
   rem,
-  useMantineTheme,
   Image,
   ActionIcon,
   useMantineColorScheme,
-  useComputedColorScheme,
+  ColorSchemeScript,
+  MantineProvider,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import cx from 'clsx';
-import { IconChevronDown, IconSun, IconMoon } from '@tabler/icons-react';
+import { IconSun, IconMoon } from '@tabler/icons-react';
 import classes from "./navbar.module.scss";
+import Lang from './language';
+import locales from '../../supportedLocales.json';
 
 const Navbar = (props: any) => {
     const { translate, pathname, locale } = props.commonProps;
     const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
     const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-    const theme = useMantineTheme();
     const { setColorScheme } = useMantineColorScheme();
-    const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
-    const isLight = (computedColorScheme === 'light');
 
     const logo = "/logo.svg";
+
+    const themeProps = {
+        variant: "default",
+        size: "md",
+        'aria-label': translate("globalNav.themeToggle"),
+    };
     
     return (
         <Box pb={120}>
-            <header className={classes.header}>
-                <Group justify="space-between" h="100%">
+            <ColorSchemeScript defaultColorScheme="dark" />
+            <MantineProvider defaultColorScheme="dark">
+                <header className={classes.header}>
+                    <Group justify="space-between" h="100%">
 
-                <Group h="100%" gap={0} visibleFrom="sm">
-                    <a style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-                        <Image src={logo} h={48} w={48} alt={"Campground"} />
-                    </a>
+                    <Group h="100%" gap={0} visibleFrom="sm">
+                        <a style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+                            <Image src={logo} h={48} w={48} alt={"Campground"} />
+                        </a>
+                        <a href="#" className={classes.link}>{translate("globalNav.download")}</a>
+                        <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
+
+                        </HoverCard>
+                        <a href="#" className={classes.link}>{translate("globalNav.blog")}</a>
+                        <a href="#" className={classes.link}>{translate("globalNav.support")}</a>
+                    </Group>
+                    <Group visibleFrom="sm" gap={12}>
+                        <Button variant="default">{translate("globalNav.login")}</Button>
+                        <Button>{translate("globalNav.register")}</Button>
+
+                        <ActionIcon onClick={() => setColorScheme('light')} {...themeProps} lightHidden>
+                            <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+                        </ActionIcon>
+                        <ActionIcon onClick={() => setColorScheme('dark')} {...themeProps} darkHidden>
+                            <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+                        </ActionIcon>
+
+                        <Lang data={locales} defaultValue={locale} pathname={pathname} translate={translate} />
+
+                    </Group>
+
+                    <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+                    </Group>
+                </header>
+
+                <Drawer opened={drawerOpened} onClose={closeDrawer} size="100%" padding="md" hiddenFrom="sm" zIndex={1000000}>
+                    <Image src={logo} h={48} w={48} alt="Campground" />
+                    <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+                    <Divider my="sm" />
+
                     <a href="#" className={classes.link}>{translate("globalNav.download")}</a>
-                    <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-
-                    </HoverCard>
                     <a href="#" className={classes.link}>{translate("globalNav.blog")}</a>
                     <a href="#" className={classes.link}>{translate("globalNav.support")}</a>
-                </Group>
-                <Group visibleFrom="sm" gap={12}>
-                    <Button variant="default">{translate("globalNav.login")}</Button>
-                    <Button>{translate("globalNav.register")}</Button>
-                    <ActionIcon
-                        onClick={() => setColorScheme(isLight ? 'dark' : 'light')}
-                        variant="default"
-                        size="md"
-                        aria-label="Toggle color scheme"
-                    >
-                        {isLight
-                            ? <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
-                            : <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
-                        }
-                    </ActionIcon>
-                </Group>
 
-                <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
-                </Group>
-            </header>
+                    <Divider my="sm" />
 
-            <Drawer opened={drawerOpened} onClose={closeDrawer} size="100%" padding="md" hiddenFrom="sm" zIndex={1000000}>
-                <Image src={logo} h={48} w={48} alt="Campground" />
-                <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
-                <Divider my="sm" />
-
-                <a href="#" className={classes.link}>{translate("globalNav.download")}</a>
-                <a href="#" className={classes.link}>{translate("globalNav.blog")}</a>
-                <a href="#" className={classes.link}>{translate("globalNav.support")}</a>
-
-                <Divider my="sm" />
-
-                <Group justify="center" grow pb="xl" px="md">
-                    <Button variant="default">{translate("globalNav.login")}</Button>
-                    <Button>{translate("globalNav.register")}</Button>
-                </Group>
-                </ScrollArea>
-            </Drawer>
+                    <Group justify="center" grow pb="xl" px="md">
+                        <Button variant="default">{translate("globalNav.login")}</Button>
+                        <Button>{translate("globalNav.register")}</Button>
+                    </Group>
+                    </ScrollArea>
+                </Drawer>
+            </MantineProvider>
+            
         </Box>
     );
 };

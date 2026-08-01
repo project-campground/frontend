029ce8896f8da5d4cb5e7694b9c8fec20409ba46
PrettyGoodName <31628630+IdkGoodName@users.noreply.github.com>
2026-08-01 21:44:15 +0300

    feat(app): add steppers and moving between pages in getting started page

--- sites/app/src/routes/(global)/ProfileSetup.svelte
+++ sites/app/src/routes/(global)/ProfileSetup.svelte
@@ -2,21 +2,53 @@
 	lang="ts"
 	module
 >
-	import { defineMessages } from '@formatjs/svelte-intl';
+	import { defineMessage, defineMessages } from '@formatjs/svelte-intl';
 
 	const localeMessages = defineMessages({
 		welcomeHeader: {
 			id: 'app.createProfile.welcome.header',
 			defaultMessage: 'Welcome to Campground!',
-			description: 'Profile creation welcome header',
+			description: 'Getting started welcome header',
 		},
 		welcomeDescription: {
 			id: 'app.createProfile.welcome.desc',
 			defaultMessage:
 				"Before we start, you need to create your profile for Campground. Don''t worry, this is a single time only process.",
-			description: 'Profile creation welcome description',
+			description: 'Getting started welcome description',
+		},
+		step: {
+			id: 'app.createProfile.step',
+			defaultMessage: 'Step {count, number}',
+			description: 'Getting started nth step',
 		},
 	});
+	const stepTitles = [
+		defineMessage({
+			id: 'app.createProfile.steps.welcome',
+			defaultMessage: 'Welcome',
+			description: 'Welcome step in the create profile stepper component',
+		}),
+		defineMessage({
+			id: 'app.createProfile.steps.appearance',
+			defaultMessage: 'Appearance',
+			description: 'Appearance step in the create profile stepper component',
+		}),
+		defineMessage({
+			id: 'app.createProfile.steps.profile',
+			defaultMessage: 'Profile',
+			description: 'Profile step in the create profile stepper component',
+		}),
+		defineMessage({
+			id: 'app.createProfile.steps.profile',
+			defaultMessage: 'Socials',
+			description: 'Socials step in the create profile stepper component',
+		}),
+		defineMessage({
+			id: 'app.createProfile.steps.final',
+			defaultMessage: 'Done',
+			description: 'Final step in the create profile stepper component',
+		}),
+	];
 </script>
 
 <script lang="ts">
@@ -30,43 +62,94 @@
 		PagePlaceholderIcon,
 		Para,
 		Stack,
+		Stepper,
 	} from '@campground/ui';
-	import { IconArrowRight } from '@tabler/icons-svelte';
+	import { IconArrowLeft, IconArrowRight, IconCheck } from '@tabler/icons-svelte';
+
+	let step: number = $state(0);
+	let maxSteps: number = $state(5);
 </script>
 
 <div class="container">
-	<Paged.Root
-		fullHeight
-		index={0}
-		count={8}
-	>
-		<Paged.Item>
-			<FlexCenter>
-				<PagePlaceholder icon={PagePlaceholderIcon.Welcome}>
-					{#snippet title()}
-						<FormattedMessage {...localeMessages.welcomeHeader} />
-					{/snippet}
-					<Stack
-						align="center"
-						gap={2}
-					>
+	<div class="content">
+		<Paged.Root
+			fullHeight
+			index={step}
+			count={maxSteps}
+		>
+			<Paged.Item>
+				<FlexCenter>
+					<PagePlaceholder icon={PagePlaceholderIcon.Welcome}>
+						{#snippet title()}
+							<FormattedMessage {...localeMessages.welcomeHeader} />
+						{/snippet}
 						<Para>
 							<FormattedMessage {...localeMessages.welcomeDescription} />
 						</Para>
-						<Button>
-							<FormattedMessageGlobal id="common.next" />
-							<IconArrowRight />
-						</Button>
-					</Stack>
-				</PagePlaceholder>
-			</FlexCenter>
-		</Paged.Item>
-	</Paged.Root>
+					</PagePlaceholder>
+				</FlexCenter>
+			</Paged.Item>
+		</Paged.Root>
+	</div>
+	<Stack
+		align="center"
+		gap={5}
+	>
+		<Stack
+			direction="row"
+			gap={2}
+		>
+			<Button
+				disabled={step < 1}
+				onclick={() => step--}
+			>
+				<IconArrowLeft />
+				<FormattedMessageGlobal id="common.back" />
+			</Button>
+			<Button
+				disabled={step == maxSteps - 1}
+				onclick={() => step++}
+			>
+				<FormattedMessageGlobal id="common.next" />
+				<IconArrowRight />
+			</Button>
+		</Stack>
+		<Stepper.Root
+			active={step}
+			size="lg"
+		>
+			{#each stepTitles as stepTitle, i (i)}
+				<Stepper.Step color="success">
+					{#snippet icon()}
+						<IconCheck />
+					{/snippet}
+					<Para
+						level="sub0"
+						lineHeight="1rem"
+					>
+						Step {i + 1}
+					</Para>
+					<Para
+						level="h3"
+						lineHeight="1rem"
+						tMargin="sm"
+					>
+						<FormattedMessage {...stepTitle} />
+					</Para>
+				</Stepper.Step>
+			{/each}
+		</Stepper.Root>
+	</Stack>
 </div>
 
 <style lang="scss">
 	.container {
-		padding: 8rem 12rem;
+		display: flex;
+		flex-direction: column;
+		padding: 10rem 16rem;
 		height: 100%;
+	}
+	.content {
+		flex: 1;
 	}
 </style>

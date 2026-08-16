61e74a2f2022d2090e69d262b73e2197bc7bd97f
PrettyGoodName <31628630+IdkGoodName@users.noreply.github.com>
2026-08-17 00:24:22 +0300

    fix(svelte-config): use actual package name for components

--- packages/svelte-config/src/index.js
+++ packages/svelte-config/src/index.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'node:path';
+
+const mostTopDir = path.join(import.meta.dirname, '../../..');
 
 /** @type {(packageName: string) => import('@sveltejs/kit').Config} */
 const createConfig = (packageName) => ({
@@ -7,7 +10,18 @@ const createConfig = (packageName) => ({
 	compilerOptions: {
 		experimental: { async: true },
 		// Makes theming a bit more predictable
-		cssHash: ({ name }) => `${packageName}-${name.replace('+', '_')}`,
+		cssHash: ({ name, filename }) => {
+			const componentPackage =
+				filename.startsWith('src') ? packageName : filename.slice(mostTopDir.length + 1).split('/')[1];
+
+			const dirName = filename.split('/').slice(-2)[0]?.split('=')[0];
+
+			const componentDirName = dirName?.slice(
+				dirName?.startsWith('(') || dirName?.startsWith('[') ? 1 : 0,
+				dirName?.endsWith(']') || dirName?.endsWith(')') ? -1 : undefined,
+			);
+			return `${componentPackage}-${componentDirName}-${name.startsWith('+') ? name.slice(1) : name}`;
+		},
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true),
 	},

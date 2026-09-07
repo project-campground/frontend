d7a35fc53747d9aa01f7557616a0372cf6c5836f
PrettyGoodName <31628630+IdkGoodName@users.noreply.github.com>
2026-09-07 19:23:59 +0300

    chore(app): remove more of ported TSX files

--- sites/app/src/lib/draggable/util.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react';
-import { useDragDrop } from './context';
-
-export function useDragging() {
-	const dragContext = useDragDrop();
-	return useMemo(() => dragContext.draggableId !== null, [dragContext.draggableId]);
-}
